49a816bcc6b0dd0d74994aa21e4c7afcf3d46080
Ug0510 <85778092+Ug0510@users.noreply.github.com>
2023-06-09 21:15:31 +0530

    Update Q1. Java Program to Print an Integer (Entered by the User).java

--- Q1. Java Program to Print an Integer (Entered by the User).java
+++ Q1. Java Program to Print an Integer (Entered by the User).java
@@ -1,1 +1,12 @@
-hi
+#Java Program to Print an Integer (Entered by the User)
+  
+import java.util.Scanner;
+
+class FirstProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("You entered "+num);
+    }
+}
