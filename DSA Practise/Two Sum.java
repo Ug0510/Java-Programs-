f186967adf5e259c03abad9d63afaa89bd88ae0f
Ug0510 <85778092+Ug0510@users.noreply.github.com>
2025-03-17 00:05:26 +0530

    Update Two Sum.java

--- DSA Practise/Two Sum.java
+++ DSA Practise/Two Sum.java
@@ -1,1 +1,29 @@
+import java.util.HashMap;
 
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // If the complement exists in the map, return the indices
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            // Store the current number and its index
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No solution found");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
